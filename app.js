0f5e11c486ffd33be789d729ebac6a1b5f7d3110
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 22:09:08 +0300

    feat: рефакторинг

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const path = require('path');
 const { usersRouter, cardsRouter, notFoundRouter } = require('./routes');
 
 const app = express();
+app.disable('x-powered-by');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -14,12 +17,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const { PORT = 3000 } = process.env;
 
-app.disable('x-powered-by');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/users/', usersRouter);
-app.use('/cards/', cardsRouter);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5f53d7dad06292ef2885b678',
+  };
+
+  next();
+});
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 app.use('/*', notFoundRouter);
 
 app.listen(PORT, () => {
   console.log(`Приложение слушает порт: ${PORT}`);
 });
+
+/*
+5f53d7dad06292ef2885b678
+*/
