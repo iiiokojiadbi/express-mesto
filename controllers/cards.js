cd0ebd4c9c52c0133fcb07a692afc48e95bed8af
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 22:03:57 +0300

    feat: реализовать контроллеры для routerCards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,17 +1,44 @@
-const path = require('path');
-const getFile = require('../helpers');
+// const path = require('path');
+// const getFile = require('../helpers');
 
-const getCards = (req, res) => getFile(path.join(__dirname, '..', 'data', 'cards.json'))
-  .then((data) => {
-    res
-      .status(200)
-      .send(JSON.parse(data));
-  }).catch((error) => {
-    res
-      .status(500)
-      .send({ message: `Произошла ошибка ${error}` });
-  });
+// const getCards = (req, res) => getFile(path.join(__dirname, '..', 'data', 'cards.json'))
+//   .then((data) => {
+//     res
+//       .status(200)
+//       .send(JSON.parse(data));
+//   }).catch((error) => {
+//     res
+//       .status(500)
+//       .send({ message: `Произошла ошибка ${error}` });
+//   });
+
+const { Card } = require('../models');
+
+const getAllCards = (req, res) => {
+  Card.find({})
+    .populate('user')
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+const deleteCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndRemove(cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
 
 module.exports = {
-  getCards,
+  getAllCards,
+  createCard,
+  deleteCard,
 };
