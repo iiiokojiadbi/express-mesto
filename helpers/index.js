38be1b333fb8f40ba48e23eced3c9ffd65bd4e45
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 21:30:13 +0300

    feat: удалить метод чтения файла

--- helpers/index.js
+++ helpers/index.js
@@ -1,14 +1,9 @@
-const fs = require('fs').promises;
-
 const {
   validateUrl,
   validateText,
 } = require('./validators');
 
-const getFile = (file) => fs.readFile(file);
-
 module.exports = {
-  getFile,
   validateUrl,
   validateText,
 };
