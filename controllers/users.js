470a20c53767810363eee837a616bcbffa581713
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 21:28:51 +0300

    feat: переписать роут

--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,29 @@
-const path = require('path');
-const getFile = require('../helpers');
+const { User } = require('../models');
 
-const getAllUsers = (req, res) => getFile(path.join(__dirname, '..', 'data', 'users.json'))
-  .then((data) => res
-    .status(200)
-    .send(JSON.parse(data))).catch((error) => res
-    .status(500)
-    .send({ message: `Произошла ошибка ${error}` }));
+const getAllUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send({ data: users }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
 
 const getUser = (req, res) => {
-  getFile(path.join(__dirname, '..', 'data', 'users.json')).then((data) => {
-    const currentUser = JSON.parse((data)).find((user) => user._id === req.params.id);
-    if (!currentUser) {
-      res
-        .status(404)
-        .send({ message: 'Нет пользователя с таким id' });
-    }
-    res
-      .status(200)
-      .send(currentUser);
-  }).catch((error) => {
-    res
-      .status(500)
-      .send({ message: `Произошла ошибка ${error}` });
-  });
+  const { user } = req.params;
+
+  User.findById(user)
+    .then((userData) => res.status(200).send({ data: userData }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
 };
 
 module.exports = {
   getAllUsers,
   getUser,
+  createUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getAllUsers, getUser } = require('../controllers/users.js');
+const { getAllUsers, getUser, createUser } = require('../controllers/users.js');
 
 const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
-
+userRouter.post('/', createUser);
 userRouter.get('/:id', getUser);
 
 module.exports = userRouter;
