70abc73a05920bd540f665d2e6ab28f05da522fb
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 20:08:20 +0300

    feat: сделать валидацию для текста и url

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,14 @@
 const fs = require('fs').promises;
 
+const {
+  validateUrl,
+  validateText,
+} = require('./validators');
+
 const getFile = (file) => fs.readFile(file);
 
-module.exports = getFile;
+module.exports = {
+  getFile,
+  validateUrl,
+  validateText,
+};
