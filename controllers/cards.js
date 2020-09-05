8740a83ea61aeaa29ccca90fcab3af271c9784e5
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 22:30:49 +0300

    fix: удалить комменты

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,17 +1,3 @@
-// const path = require('path');
-// const getFile = require('../helpers');
-
-// const getCards = (req, res) => getFile(path.join(__dirname, '..', 'data', 'cards.json'))
-//   .then((data) => {
-//     res
-//       .status(200)
-//       .send(JSON.parse(data));
-//   }).catch((error) => {
-//     res
-//       .status(500)
-//       .send({ message: `Произошла ошибка ${error}` });
-//   });
-
 const { Card } = require('../models');
 
 const getAllCards = (req, res) => {
