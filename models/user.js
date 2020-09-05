7ee603141ea11bcc5bac9b71dc4a596537468303
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 20:20:54 +0300

    feat: создать схемы для БД

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { validateText, validateUrl } = require('../helpers/validators');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator: (name) => validateText(name),
+      message: 'Введите правильный текст',
+    },
+  },
+  about: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (name) => validateText(name),
+      message: 'Введите правильный текст',
+    },
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => validateUrl(link),
+      message: 'Введите правильный url',
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
