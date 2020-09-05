71e61b74333a385327ddfaa7b6ccc3c8213a3b75
iiiokojiadbi <iiiokojiadbi@yandex.ru>
2020-09-05 20:21:39 +0300

    feat: подключить mongodb ;(

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const { usersRouter, cardsRouter, notFoundRouter } = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const { PORT = 3000 } = process.env;
 
